ba2aa16d5eeab2ee45e5a014aaf75978c7592ab9
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2026-01-20 01:58:51 +0530

    test: add main program to verify database integration

--- cmd/jobit/main.go
+++ cmd/jobit/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	store "github.com/MrMoneyInTheBank/jobit/internal/store"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	db, err := store.OpenDB(nil)
+	if err != nil {
+		log.Fatalf("Could not open database: %v", err)
+	}
+	defer store.CloseDB(db)
+	store.InitDB(db)
+
+	var seedCount int
+	fmt.Print("How many applications to seed: ")
+	fmt.Scanln(&seedCount)
+	err = store.SeedDB(db, seedCount)
+	if err != nil {
+		log.Fatalf("Could not seed database: %v", err)
+	}
+
+	fmt.Printf("Seeded database.\n\n")
+	apps, err := store.GetJobApplications(db)
+	fmt.Printf("Got %v applications\n", len(apps))
+
+	for _, app := range apps {
+		fmt.Printf(
+			"id: %v, Company Name: %v, Position: %v\n",
+			app.ID,
+			app.CompanyName,
+			app.Position)
+	}
 }
