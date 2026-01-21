11f73155c63085a06aa496a2312046ce3efe9576
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2026-01-21 22:39:16 +0530

    chore: delete seeding logic from jobit module

--- cmd/jobit/main.go
+++ cmd/jobit/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	store "github.com/MrMoneyInTheBank/jobit/internal/store"
@@ -15,23 +14,8 @@ func main() {
 	defer store.CloseDB(db)
 	store.InitDB(db)
 
-	var seedCount int
-	fmt.Print("How many applications to seed: ")
-	fmt.Scanln(&seedCount)
-	err = store.SeedDB(db, seedCount)
 	if err != nil {
-		log.Fatalf("Could not seed database: %v", err)
 	}
 
-	fmt.Printf("Seeded database.\n\n")
-	apps, err := store.GetJobApplications(db)
-	fmt.Printf("Got %v applications\n", len(apps))
-
-	for _, app := range apps {
-		fmt.Printf(
-			"id: %v, Company Name: %v, Position: %v\n",
-			app.ID,
-			app.CompanyName,
-			app.Position)
 	}
 }
