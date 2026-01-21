09d8659369c4151b756500df425c51a23056995c
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2026-01-21 22:38:36 +0530

    feat: generate initial tea.Model function

--- internal/tui/job_list.go
+++ internal/tui/job_list.go
@@ -1,7 +1,13 @@
 package tui
+
 import (
+	"strconv"
+
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/MrMoneyInTheBank/jobit/internal/model"
 )
 
 type JobList struct {
@@ -31,3 +37,26 @@ func (jl *JobList) View() string {
 	return baseStyle.Render(jl.JobsTable.View()) + "\n"
 }
 
+func InitJobList(jobs []model.JobApplication) JobList {
+	columns := []table.Column{
+		{Title: "ID", Width: 5},
+		{Title: "Company Name", Width: 20},
+		{Title: "Position", Width: 20},
+	}
+	rows := make([]table.Row, len(jobs))
+
+	for idx, job := range jobs {
+		rows[idx] = table.Row{
+			strconv.Itoa(int(job.ID)),
+			job.CompanyName,
+			job.Position,
+		}
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+	)
+
+	return JobList{t}
+}
